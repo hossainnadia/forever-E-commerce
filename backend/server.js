ddc96050634a6941c69d9cf257490c1cef593965
hossainnadia <anchowdhury3562@gmail.com>
2025-08-19 19:05:05 +0600

    changing code

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,17 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+// middlewares
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"], // দুইটা allow করলাম
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // api endpoints
 app.use("/api/user", userRouter);
